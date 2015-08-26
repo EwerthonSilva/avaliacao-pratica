f3e72c07a10cee8868d65de9cb7c66b8339907e8
EwerthonSilva <ewerthon.silva@s2it.colm.br>
2015-08-26 14:24:01 -0300

    Criação do model Contribuinte e Dependentes com seus respectivos relacionamentos

--- src/main/java/br/com/s2it/incubadora/model/Contribuinte.java
+++ src/main/java/br/com/s2it/incubadora/model/Contribuinte.java
@@ -1,7 +1,105 @@
 package br.com.s2it.incubadora.model;
 
-public class Contribuinte {
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "contribuinte")
+public class Contribuinte implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	public Contribuinte(){
+		
+	}
 	
+	public Contribuinte(int cod){
+		this.cod = cod;
+	}
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int cod;
+
+	@Column(name="nome")
+	private String nome;
+	@Column(name="cpf")	
+	private String cpf;
+	@Column(name="rg")
+	private String rg;
+	@Column(name="rendaAnual")
+	private BigDecimal rendaAnual;
+	@Column(name="dataNasc")
+	private Date dataNasc;
+	
+	private Set<Dependentes> dependentes;
+	
+
+	@OneToMany(cascade = ALL, mappedBy = "contribuinte")
+	public Set<Dependentes> getDependentes() {
+		return dependentes;
+	}
+
+	// public void setDependentes(Dependentes dependentes) {
+	// this.dependentes = (Set<Dependentes>) dependentes;
+	// }
+
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(Date dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public void setCod(int cod) {
+		this.cod = cod;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public BigDecimal getRendaAnual() {
+		return rendaAnual;
+	}
+
+	public void setRendaAnual(BigDecimal rendaAnual) {
+		this.rendaAnual = rendaAnual;
+	}
+
 }
--- src/main/java/br/com/s2it/incubadora/model/Dependentes.java
+++ src/main/java/br/com/s2it/incubadora/model/Dependentes.java
@@ -1,5 +1,98 @@
 package br.com.s2it.incubadora.model;
 
-public class Dependentes {
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "dependentes")
+
+public class Dependentes implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public Dependentes() {
+
+	}
+
+	public Dependentes(int cod) {
+		this.cod = cod;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int cod;
+
+	@Column(name = "nome")
+	private String nome;
+	@Column(name = "cpf")
+	private String cpf;
+	@Column(name = "rg")
+	private String rg;
+	@Column(name = "rendaAnual")
+	private BigDecimal rendaAnual;
+	@Column(name = "dataNasc")
+	private Date dataNasc;
+
+	private Contribuinte contribuinte;
+
+	@ManyToOne
+	@JoinColumn(name = "codContribuinte")
+	public Contribuinte getContribuinte() {
+		return contribuinte;
+	}
+
+	// public void setContribuinte(Contribuinte contribuinte) {
+	// this.contribuinte = contribuinte;
+	// }
+	//
+	public int getCod() {
+		return cod;
+	}
+
+	public void setCod(int cod) {
+		this.cod = cod;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(Date dataNasc) {
+		this.dataNasc = dataNasc;
+	}
 
 }
